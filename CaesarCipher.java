06ecdff11fc37d9ef47658be00ec77ada27e667f
milkaotieno <milka.otieno@student.moringaschool.com>
2022-02-22 01:27:57 +0300

    delete unnecessary files

--- CaesarCipher.java
+++
@@ -1,8 +0,0 @@
-public class CaesarCipher.java{
-public static void main(String[]args){
-//        alt + shift + t (you may need to try
-//        ctrl + shift + t (Ubuntu) or
-//        command + shift + t
-
-        }
-        }
