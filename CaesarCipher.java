2f8466e0518aaec11937b97b4879462ea5a2df4d
milkaotieno <milka.otieno@student.moringaschool.com>
2022-02-22 00:31:07 +0300

    create test file

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,5 +1,8 @@
-public class CaesarCipher.java {
-    public static void main(){
+public class CaesarCipher.java{
+public static void main(String[]args){
+//        alt + shift + t (you may need to try
+//        ctrl + shift + t (Ubuntu) or
+//        command + shift + t
 
         }
         }
